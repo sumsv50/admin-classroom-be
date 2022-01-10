16905e54ca22161538e05aaeb6bd37f1ed3a5174
sumgu <sum.dv@geekup.vn>
2022-01-10 21:19:47 +0700

    hotfix/add field createdAt of user in api get user list

--- components/users/usersService.js
+++ components/users/usersService.js
@@ -3,7 +3,7 @@ const { Users } = db;
 
 module.exports.getAllUsers = async function() {
   const users = await Users.findAll({
-    attributes: ['id', 'avatar', 'email', ['student_id', 'studentId'], 'isBanned'],
+    attributes: ['id', 'avatar', 'email', ['student_id', 'studentId'], 'isBanned', 'createdAt'],
     order: [
       // Will escape title and validate DESC against a list of valid direction parameters
       ['email', 'ASC'],
