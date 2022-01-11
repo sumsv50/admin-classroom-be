52d75e5bbd55a96841e649b390966e77be14dbc1
sumgu <sum.dv@geekup.vn>
2022-01-11 21:20:21 +0700

    add field name in api get user list

--- components/users/usersService.js
+++ components/users/usersService.js
@@ -4,7 +4,7 @@ const { Users, Classes, User_Class } = db;
 
 module.exports.getAllUsers = async function() {
   const users = await Users.findAll({
-    attributes: ['id', 'avatar', 'email', ['student_id', 'studentId'], 'isBanned', 'createdAt'],
+    attributes: ['id', 'name', 'avatar', 'email', ['student_id', 'studentId'], 'isBanned', 'createdAt'],
     order: [
       // Will escape title and validate DESC against a list of valid direction parameters
       ['email', 'ASC'],
